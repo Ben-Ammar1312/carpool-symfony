9918b736e9011a22396450cd5b7b348d34942444
OuniYoussef <ouniyoussef619@gmail.com>
2025-01-05 22:18:08 +0100

    syntax fixes

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -2,16 +2,13 @@
 
 namespace App\Form;
 
-<<<<<<< HEAD
+
 use App\Entity\Annonce;
 use App\Entity\Reservation;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-=======
-use assets\Entity\Annonce;use assets\Entity\Reservation;use Symfony\Bridge\Doctrine\Form\Type\EntityType;use Symfony\Component\Form\AbstractType;use Symfony\Component\Form\FormBuilderInterface;use Symfony\Component\OptionsResolver\OptionsResolver;
->>>>>>> 2770c5b04fde1c00f85c9278b3448a36307b2bca
+
 
 class ReservationType extends AbstractType
 {
@@ -31,10 +28,5 @@ class ReservationType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Reservation::class,
-        ]);
-    }
+
 }
